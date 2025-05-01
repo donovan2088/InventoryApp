b324a1b2e6eced76ae8ba34da34b651aebf4b8e2
donovan88 <alandon201288@gmail.com>
2025-05-01 14:26:24 -0600

    Creación de service para la gestión de catalogos de la InventoryApp

---
+++ gestioncatalogoservice/src/main/java/com/inventory/gestioncatalogoservice/GestioncatalogoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.gestioncatalogoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioncatalogoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioncatalogoserviceApplication.class, args);
+	}
+
+}
---
+++ gestioncatalogoservice/src/test/java/com/inventory/gestioncatalogoservice/GestioncatalogoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.gestioncatalogoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncatalogoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
